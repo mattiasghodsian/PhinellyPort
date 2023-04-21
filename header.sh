adf7a9bfdcea74bcf3b1e1067eca49e0e6a1e057
mattiasghodsian <mattiasghodsian@gmail.com>
2023-04-21 13:57:36 +0200

    Add header.sh

---
+++ header.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: Mattias Ghodsian
+# License: MIT License
+# url: https://github.com/mattiasghodsian/PhinellyPort
+
+clear
+echo -e "\033[1m\033[32m
+    ____  __    _            ____      ____             __ 
+   / __ \/ /_  (_)___  ___  / / /_  __/ __ \____  _____/ /_
+  / /_/ / __ \/ / __ \/ _ \/ / / / / / /_/ / __ \/ ___/ __/
+ / ____/ / / / / / / /  __/ / / /_/ / ____/ /_/ / /  / /_  
+/_/   /_/ /_/_/_/ /_/\___/_/_/\__, /_/    \____/_/   \__/  
+                             /____/           
+                                                                        
+\033[0m"
+echo -e "\033[32m$(printf '%*s' $(((59/2)+9)) "PhinellyPort v1.0")\033[0m"
+echo -e "\033[32m$(printf '%*s' $(((59/2)+24)) "https://github.com/mattiasghodsian/PhinellyPort")\033[0m"
+echo ""
